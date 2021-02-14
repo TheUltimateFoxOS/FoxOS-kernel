1081333199ce4103db8d01044e705f0a6c869bec
root <root@janick.localdomain>
2021-02-14 21:54:38 +0100

    indent fix

--- load_gdt.asm
+++ load_gdt.asm
@@ -3,10 +3,10 @@ load_gdt:
 	lgdt [rdi]
 	mov ax, 0x10
 	mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
 
 	pop rdi
 	mov rax, 0x08
