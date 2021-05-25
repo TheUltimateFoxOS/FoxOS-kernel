d285cba5efbf2275cd8a68605383bd520715f3c4
root <root@DESKTOP-OQC4PB9.localdomain>
2021-05-25 20:22:50 +0200

    saving all regs

--- src/include/interrupts/interrupts.h
+++ src/include/interrupts/interrupts.h
@@ -17,6 +17,14 @@ struct s_registers {
 	uint64_t cr3;
 	uint64_t cr2;
 	uint64_t cr0;
+	uint64_t r15;
+	uint64_t r14;
+	uint64_t r13;
+	uint64_t r12;
+	uint64_t r11;
+	uint64_t r10;
+	uint64_t r9;
+	uint64_t r8;
 	uint64_t rdi;
 	uint64_t rsi;
 	uint64_t rbp;
