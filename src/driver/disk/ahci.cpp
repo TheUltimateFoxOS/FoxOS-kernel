3f504bdb239bcb6bc148c21e55428d6a7b06cceb
ImDaBigBoss <67973871+ImDaBigBoss@users.noreply.github.com>
2021-05-25 19:10:54 +0200

    Added a write command to AHCI

--- src/driver/disk/ahci.cpp
+++ src/driver/disk/ahci.cpp
@@ -112,7 +112,6 @@ void AHCI_port::read(uint64_t sector, uint32_t sector_count, void* buffer) {
 		spin ++;
 	}
 	if (spin == 1000000) {
-		//return false;
 		return;
 	}
 
@@ -157,12 +156,69 @@ void AHCI_port::read(uint64_t sector, uint32_t sector_count, void* buffer) {
 	while (true) {
 		if((hba_port->command_issue == 0)) break;
 		if(hba_port->interrupt_status & HBA_PxIS_TFES) {
-			//return false;
 			return;
 		}
 	}
 
-	//return true;
+	return;
+}
+
+void AHCI_port::write(uint64_t sector, uint32_t sector_count, void* buffer) {
+	uint64_t spin = 0;
+	while ((hba_port->task_file_data & (ATA_DEV_BUSY | ATA_DEV_DRQ)) && spin < 1000000) {
+		spin ++;
+	}
+	if (spin == 1000000) {
+		return;
+	}
+
+	g_page_table_manager.map_memory(buffer, buffer);
+
+	uint32_t sector_low = (uint32_t) sector;
+	uint32_t sector_high = (uint32_t) (sector >> 32);
+
+	hba_port->interrupt_status = (uint32_t)-1; // Clear pending interrupt bits
+
+	HBA_command_header* cmd_header = (HBA_command_header*)hba_port->command_list_base;
+	cmd_header->command_fis_length = sizeof(FIS_REG_H2D)/ sizeof(uint32_t); //command FIS size;
+	cmd_header->write = 1; // Indicate a write
+	cmd_header->prdt_length = 1;
+
+	HBA_command_table* commandTable = (HBA_command_table*)(cmd_header->command_table_base_address);
+	memset(commandTable, 0, sizeof(HBA_command_table) + (cmd_header->prdt_length-1)*sizeof(HBA_PRDT_entry));
+
+	commandTable->prdt_entry[0].data_base_address = (uint32_t)(uint64_t)buffer;
+	commandTable->prdt_entry[0].data_base_address_upper = (uint32_t)((uint64_t)buffer >> 32);
+	commandTable->prdt_entry[0].byte_count = (sector_count << 9) - 1; // 512 bytes per sector
+	commandTable->prdt_entry[0].interrupt_on_completion = 1;
+
+	FIS_REG_H2D* cmd_fis = (FIS_REG_H2D*)(&commandTable->command_fis);
+
+	cmd_fis->fis_type = FIS_TYPE_REG_H2D;
+	cmd_fis->command_control = 1; // Command
+	cmd_fis->command = ATA_CMD_WRITE_DMA_EX;
+
+	cmd_fis->lba0 = (uint8_t)sector_low;
+	cmd_fis->lba1 = (uint8_t)(sector_low >> 8);
+	cmd_fis->lba2 = (uint8_t)(sector_low >> 16);
+	cmd_fis->lba3 = (uint8_t)sector_high;
+	cmd_fis->lba4 = (uint8_t)(sector_high >> 8);
+	cmd_fis->lba5 = (uint8_t)(sector_high >> 16);
+
+	cmd_fis->device_register = 1<<6; //LBA mode
+
+	cmd_fis->count_low = sector_count & 0xFF;
+	cmd_fis->count_high = (sector_count >> 8) & 0xFF;
+
+	hba_port->command_issue = 1;
+
+	while (true) {
+		if((hba_port->command_issue == 0)) break;
+		if(hba_port->interrupt_status & HBA_PxIS_TFES) {
+			return;
+		}
+	}
+
 	return;
 }
 
