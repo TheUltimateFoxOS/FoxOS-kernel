a2e13acb7089bc9b66ce0d08e437a5805fbcedb9
root <root@wrrskofr1ipad11.localdomain>
2021-06-12 22:22:59 +0200

    toolchain: build libgcc without red zone

--- toolchain.sh
+++ toolchain.sh
@@ -21,7 +21,6 @@ tar xf gcc-10.2.0.tar.gz
 unzip nasm-2.15.05
 
 echo "Compiling..."
-
 mkdir binutils-build
 cd binutils-build
 ../binutils-2.35.1/configure --target=$TARGET --program-prefix=$PROG_PREFIX --with-sysroot --disable-nls --disable-werror --prefix=$PREFIX
@@ -32,13 +31,17 @@ cd $BUILD_DIR
 
 cd gcc-10.2.0
 ./contrib/download_prerequisites
+echo "MULTILIB_OPTIONS += mno-red-zone" > gcc/config/i386/t-x86_64-elf
+echo "MULTILIB_DIRNAMES += no-red-zone" >> gcc/config/i386/t-x86_64-elf
+
+sed -i 's/x86_64-\*-elf\*)/x86_64-\*-elf\*)\n	tmake_file="\${tmake_file} i386\/t-x86_64-elf" # include the new multilib configuration/' gcc/config.gcc
+
 cd ..
 mkdir gcc-build
 cd gcc-build
 ../gcc-10.2.0/configure --target=$TARGET --program-prefix=$PROG_PREFIX --prefix="$PREFIX" --disable-nls --enable-languages=c++ --without-headers
 make all-gcc -j $CORES
 make all-target-libgcc -j $CORES
-
 sudo make install-gcc
 sudo make install-target-libgcc
 
