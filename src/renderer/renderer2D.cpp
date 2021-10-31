de9f50de38c79742e611c56295de6cd86ed0ae96
Glowman554 <glowman434@gmail.com>
2021-10-31 22:35:07 +0100

    better scroling

--- src/renderer/renderer2D.cpp
+++ src/renderer/renderer2D.cpp
@@ -76,31 +76,10 @@ void Renderer2D::draw_overlay_mouse_cursor(uint8_t* mouse_pointer, point_t pos, 
 //#Renderer2D::scroll_down-doc: Scroll the screen down.
 void Renderer2D::scroll_down() {
 	global_renderer2D->clear_mouse_cursor(global_mouse_renderer->get_mouse_pointer(), global_mouse_renderer->mouse_position);
-	for (unsigned long y = 0; y <= 16; y++) { // clear the first line
-		for (unsigned long x = 0; x <= target_frame_buffer->width; x++) {
-			*(uint32_t*)((uint64_t)target_frame_buffer->base_address + (x*4) + (y * target_frame_buffer->pixels_per_scanline * 4)) = 0x00000000;
-		}
-	}
-	for (unsigned long y = 16; y <= target_frame_buffer->height - 1; y++) { // copy the rest
-		for (unsigned long x = 0; x <= target_frame_buffer->width; x++) {
-			uint32_t old = *(uint32_t*)((uint64_t)target_frame_buffer->base_address + (x*4) + (y * target_frame_buffer->pixels_per_scanline * 4));
-			*(uint32_t*)((uint64_t)target_frame_buffer->base_address + (x*4) + ((y - 16) * target_frame_buffer->pixels_per_scanline * 4)) = old;
-		}
-	}
-	for (unsigned long y = target_frame_buffer->height - 16; y <= target_frame_buffer->height - 1; y++) { // clear the last line
-		for (unsigned long x = 0; x <= target_frame_buffer->width; x++) {
-			*(uint32_t*)((uint64_t)target_frame_buffer->base_address + (x*4) + (y * target_frame_buffer->pixels_per_scanline * 4)) = 0x00000000;
-		}
-	}
 
-	// memset(target_frame_buffer->base_address, 0, target_frame_buffer->width * 16 * 4);
-	// memcpy(target_frame_buffer->base_address, (void*) ((uint64_t) target_frame_buffer->base_address + target_frame_buffer->width * 16 * 4), target_frame_buffer->buffer_size - target_frame_buffer->width * 16 * 4);
-
-	// for (unsigned long y = target_frame_buffer->height - 16; y <= target_frame_buffer->height - 1; y++) { // clear the last line
-	// 	for (unsigned long x = 0; x <= target_frame_buffer->width; x++) {
-	// 		*(uint32_t*)((uint64_t)target_frame_buffer->base_address + (x*4) + (y * target_frame_buffer->pixels_per_scanline * 4)) = 0x00000000;
-	// 	}
-	// }
+	memcpy((void*) (uint64_t)this->target_frame_buffer->base_address, (void*)((uint64_t)this->target_frame_buffer->base_address + (16 * this->target_frame_buffer->pixels_per_scanline * 4)), (this->target_frame_buffer->width * 4 * (this->target_frame_buffer->height - 16)));
+	memset((void*) ((uint64_t)this->target_frame_buffer->base_address + ((this->target_frame_buffer->width * 4) * (this->target_frame_buffer->height - 16))), 0, (this->target_frame_buffer->width * 4 * 16));
+	memset((void*) ((uint64_t)this->target_frame_buffer->base_address + ((this->target_frame_buffer->width * 4) * (this->target_frame_buffer->height - 16))), 0, (this->target_frame_buffer->width * 4 * 16));
 
 	// global_renderer2D->draw_overlay_mouse_cursor(global_mouse_renderer->get_mouse_pointer(), global_mouse_renderer->mouse_position, 0xffffffff);
 }
